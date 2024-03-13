15cca36c87ceefc01ba0da1ccab532ad60748d92
Eduardo Simionato Nogueira <edunogueira793@gmail.com>
2024-03-13 12:24:07 -0300

    Adição de Variaveis de destaque

--- StickerSmash/src/components/Button.js
+++ StickerSmash/src/components/Button.js
@@ -1,13 +1,13 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function Button({label, theme }) {
+export default function Button({label, theme, onPress }) {
   if (theme == "primary"){
   return (
     <View style={[styles.buttonContainer, {borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18}]}>
         <Pressable 
         style={[styles.button, { backgroundColor: '#fff'}]}
-        onPress={() => alert('Você pressionou um Botão')}
+        onPress={onPress}
         >
           <FontAwesome
             name='picture-o'
@@ -25,7 +25,7 @@ return (
   <View style={styles.buttonContainer}>
       <Pressable 
       style={styles.button}
-      onPress={() => alert('Você pressionou um Botão')}
+      onPress={onPress}
       >
           <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
